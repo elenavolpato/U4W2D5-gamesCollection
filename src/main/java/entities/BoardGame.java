455bde113b3f75fa3565fb9dadfaf9acc282b6d0
raposinha <elena@raposinha.dev>
2026-03-20 10:41:20 +0100

    finishes boardgame class

--- src/main/java/entities/BoardGame.java
+++ src/main/java/entities/BoardGame.java
@@ -1,4 +1,29 @@
 package entities;
 
-public class BoardGame {
+//Board games instead have:
+//    Number of players (range from 2 to 10)
+//    Average match duration (in minutes)
+
+public class BoardGame extends Game{
+    private int maxPlayers;
+    private int matchDuration;
+
+    public BoardGame(String gameId, String title, int year, double price, int maxPlayers, int matchDuration){
+        super(gameId,  title, year,price);
+        this.matchDuration = matchDuration;
+
+        if(maxPlayers < 2 || maxPlayers >10){
+            throw new IllegalArgumentException("Players must be between 2 and 10");
+        }
+        this.maxPlayers = maxPlayers;
+    }
+
+    public int getMaxPlayers(){
+        System.out.println("maxPlayers: " + maxPlayers);
+        return maxPlayers;
+    }
+    public int getMatchDuration(){
+        System.out.println("matchDuration: " + matchDuration);
+        return matchDuration;
+    }
 }
--- src/main/java/entities/VideoGame.java
+++ src/main/java/entities/VideoGame.java
@@ -1,4 +1,33 @@
+////Video games also have:
+////    Platform (e.g., PC, PS5, Xbox)
+////    Game length (in hours)
+////    Genre (an enum with a list of genres of your choice)
+
 package entities;
 
-public class VideoGame {
+public class VideoGame extends Game{
+    private String platform;
+    private double gameDuration;
+    private Genre genre;
+
+    public VideoGame(String gameId, String title, int year, double price, String platform, double gameDuration, Genre genre){
+        super(gameId,  title, year,price);
+        this.platform = platform;
+        this.gameDuration = gameDuration;
+        this.genre = genre;
+    }
+
+    public String getPlatform(){
+        System.out.println("Platform: " + platform);
+        return platform;
+    }
+    public double getGameDuration(){
+        System.out.println("gameDuration: " + gameDuration);
+        return gameDuration;
+    }
+    public Genre getGenre(){
+        System.out.println("Genre: " + genre);
+        return genre;
+    }
+
 }
